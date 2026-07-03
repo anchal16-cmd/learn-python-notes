c0a86441a402cae22a79ede44d5a0bd050e5c252
Aanchal Pandey <a33181748@gmail.com>
2026-07-03 20:38:57 +0530

    Add HackerRank Python solutions and update README

---
+++ hackerrank-python-solutions/001_hello_world.py
@@ -0,0 +1,5 @@
+# Platform: HackerRank
+# Problem: Say "Hello, World!" With Python
+# Link: https://www.hackerrank.com/challenges/py-hello-world/problem?isFullScreen=true
+
+print("Hello, World!")
---
+++ hackerrank-python-solutions/003_arithmetic_operators.py
@@ -0,0 +1,15 @@
+# Platform: HackerRank
+# Problem: Arithmetic Operators
+# Link: https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?isFullScreen=true
+
+def all_arithmetic_ops(num1 , num2):
+    print(num1 + num2)
+    print(num1 - num2)
+    print(num1 * num2)
+
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    all_arithmetic_ops(a , b)
+
